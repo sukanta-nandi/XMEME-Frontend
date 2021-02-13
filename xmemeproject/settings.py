25033504835b72fad4ef5980a925fd02bf6a157e
sukanta nandi <sukantanandi0@gmail.com>
2021-02-13 22:46:01 +0530

    fixed wsgi path

--- xmemeproject/settings.py
+++ xmemeproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '34@c1dgaxykf5(&9_4ufdghws_qi**1ajpm)w)(6wi&jbzym-*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['xmemebackendapp.herokuapp.com', '127.0.0.1:8000']
 
